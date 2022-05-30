53dfc4df8b98a1e7f8c401306d68a8b9736c775a
NisrokMohyono <64055397+NisrokMohyono@users.noreply.github.com>
2022-05-31 03:52:19 +0700

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -16,3 +16,15 @@ workbox.routing.registerRoute(
     cacheName: CACHE
   })
 );
+
+self.addEventListener('install', (event) => {
+  console.log('Inside the install handler:', event);
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Inside the activate handler:', event);
+});
+
+self.addEventListener(fetch, (event) => {
+  console.log('Inside the fetch handler:', event);
+});
